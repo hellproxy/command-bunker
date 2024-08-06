347417e979dacc31df1ed1c9ceb1335959077195
Harry Dent <harry.j.e.dent@gmail.com>
2024-08-06 12:37:55 +0100

    clear info

--- src/components/play/info-display.tsx
+++ src/components/play/info-display.tsx
@@ -1,14 +1,25 @@
 import { useInfoStore } from "@/stores/info";
+import { X } from "lucide-react";
 
 export const InfoDisplay = () => {
-  const info = useInfoStore((state) => state.info);
+  const [info, clear] = useInfoStore((state) => [state.info, state.clearInfo]);
 
   return (
-    <div className="flex bg-white rounded shadow-md px-4 py-4 overflow-hidden">
-      {info && (
-        <div className="flex flex-col gap-2 px-1 overflow-auto">
-          <div>{info.title}</div>
-          <div className="text-sm">{info.text}</div>
+    <div className="flex bg-white rounded shadow-md px-2 py-4 overflow-hidden">
+      {info ? (
+        <div className="flex flex-col gap-2 px-3 overflow-auto">
+          <div className="flex flex-row items-center">
+            <div className="mr-auto">{info.title}</div>
+            <button className="btn btn-gray p-0.5" onClick={clear}>
+              <X size={16} />
+            </button>
+          </div>
+          <div className="text-sm text-justify">{info.text}</div>
+        </div>
+      ) : (
+        <div className="text-gray-400 px-3">
+          Click any <span className="font-bold">bold text</span> to view more
+          info here
         </div>
       )}
     </div>
